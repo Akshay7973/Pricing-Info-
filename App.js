789ce9e97dd8503ab47afd0f3ff7a95c589a9697
Akshay7973 <84506509+Akshay7973@users.noreply.github.com>
2021-09-02 15:29:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Routing from './Components/Route';
+
+
+
+function App()
+            {
+  return (
+    <div className="App">
+     <Routing />
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ Components3/Productinfo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Productinfo = () => {
+    return (
+        <div>
+           <h1>its akshay</h1>
+            
+        </div>
+    )
+}
+
+export default Productinfo
